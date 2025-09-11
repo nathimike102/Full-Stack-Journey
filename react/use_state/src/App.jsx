883ab6fcd8146169a0bcb7d65d0b24a4e8911043
Nkosinathi Michael Sibanda <23mh1a05h9@acoe.edu.in>
2025-09-11 11:40:56 +0530

    useState hook

---
+++ react/use_state/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  }
+  return (
+    <>
+      <div className="card">
+
+        <button onClick={incrementCount}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
