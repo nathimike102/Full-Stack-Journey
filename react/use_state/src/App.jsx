6f8f67636ca01bfe639400f5b546a1ee63c970f6
Nkosinathi Michael Sibanda <23mh1a05h9@acoe.edu.in>
2025-09-18 11:36:47 +0530

    using useState filter, and distruct

--- react/use_state/src/App.jsx
+++ react/use_state/src/App.jsx
@@ -3,6 +3,15 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [Data, setData] = useState([1, 2, 3])
+
+  const addData = (value) => {
+    setData([...Data, value])
+  }
+
+  const removeData = (value) => {
+    setData(Data.filter(item => item !== value))
+  }
 
   const incrementCount = () => {
     setCount(count + 1);
@@ -10,10 +19,32 @@ function App() {
   return (
     <>
       <div className="card">
-
         <button onClick={incrementCount}>
           count is {count}
         </button>
+      </div>
+
+      <div className="card">
+        <button onClick={() => {
+          const x = prompt("Enter a number");
+          addData(Number(x));
+        }}>
+          Add Data
+        </button>
+      </div>
+
+      <div className="card">
+        <button onClick={() => {
+          const x = prompt("Enter a number");
+          removeData(Number(x));
+        }}>
+          Remove Data
+        </button>
+      </div>
+      
+      <div className="card">
+        <h2>Data List:</h2>
+        <p>{Data.join(" -> ")}</p>
       </div>
     </>
   )
