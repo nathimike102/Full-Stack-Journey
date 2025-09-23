6149c842a9f6d5d7e7e4bfccc7337cdfb12a9fb2
Nkosinathi Michael Sibanda <23mh1a05h9@acoe.edu.in>
2025-09-23 11:07:12 +0530

    use_effect hook

---
+++ react/use_effect/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    console.log('Count has changed:', count)
+  }, [count])
+
+  return (
+    <>
+      <h1>Current Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </>
+  )
+}
+
+export default App
