8d6ef201b30665997c5fa08a40ce430a6844a79c
Nkosinathi Michael Sibanda <23mh1a05h9@acoe.edu.in>
2026-01-27 13:00:45 +0530

    Feat: Made a simple login page

--- react_native/first_app/App.js
+++ react_native/first_app/App.js
@@ -1,13 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput} from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const Login = () => {
+    if(username === '' || password === '') {
+      alert("Please enter both username and password.");
+      return;
+    }
+    alert("Details Submitted!\nUsername: " + username + "\nPassword: " + password);
+    setPassword('');
+    setUsername('');
+  };
+
   return (
     <>
     <View style={styles.container}>
-      <Text style={styles.title}>First App</Text>
-      <Text style={styles.button} onPress={() => alert('Button Pressed!')}>Press Me</Text>
+      <Text style={[styles.title, {fontSize: 30}]}>Welcome</Text>
+      <Text style={styles.title}>First React Native App!</Text>
+      <View style={styles.form}>
+        <Text style={styles.label}>Username:</Text> 
+        <TextInput style={styles.input} onChangeText={text => setUsername(text)} value={username} />
+        <Text style={styles.label}>Password:</Text>
+        <TextInput style={styles.input} secureTextEntry={true} onChangeText={text => setPassword(text)} value={password} />
+      </View>
+      <Text style={styles.button} onPress={Login}>Login</Text>
       <StatusBar style="auto" />
+    </View>
+    <View>
+      <Text style={styles.footer}>nathi_mike</Text>
     </View>
     </>
   );
@@ -16,9 +39,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00ffff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  form: {
+    marginTop: 20,
+    alignItems: 'flex-start',
   },
   button: {
     marginTop: 20,
@@ -28,7 +55,27 @@ const styles = StyleSheet.create({
     color: '#fff',
   },
   title: {
-    fontSize: 24,
+    fontSize: 20,
     fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    width: 200,
+    paddingHorizontal: 10,
+    marginTop: 5,
+    backgroundColor: '#fff',
+  },
+  label: {
+    marginTop: 15,
+    fontSize: 16,
+  },
+  footer: {
+    textAlign: 'center',
+    padding : 10,
+    backgroundColor: '#00ffff',
   },
 });
